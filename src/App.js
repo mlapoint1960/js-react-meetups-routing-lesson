c8dcbe27ce39f6f65729a26ba66b7dc3e9560c77
Mark LaPoint <marklapoint@gmail.com>
2021-06-22 11:14:11 -0600

    meetups routing react tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupsPage from './pages/NewMeetups';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupsPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,18 @@
+import classes from './MeetupList.module.css';
+
+function MeetupList(props) {
+    return (
+        <ul className={classes.list}>
+            {props.meetups.map((meetup) => (
+                <MeetupItem
+                    key={meetup.id} 
+                    id={meetup.id}
+                    image={meetup.image}
+                    title={meetup.title}
+                />
+            ))}
+        </ul>
+    );
+}
+
+export default MeetupList;
