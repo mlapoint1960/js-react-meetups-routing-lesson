3a5bca35bc93a837a9d44bec46c3b235f205a916
Mark LaPoint <marklapoint@gmail.com>
2021-12-17 14:08:33 -0700

    fixes
    something went wrong

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import Card from '../ui/Card';
 import classes from './NewMeetupForm.module.css';
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
     const titleInputRef = useRef();
     const imageInputRef = useRef();
     const addressInputRef = useRef();
@@ -23,9 +23,8 @@ function NewMeetupForm() {
             description: enteredDescription,
         };
 
-        console.log(meetupData);
+        props.onAddMeetup(meetupData);
     }
-
     return (
     <Card>
         <form className={classes.form} onSubmit={submitHandler}>
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,27 @@
+import { useHistory } from 'react-router';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+    const history = useHistory();
+    function onAddMeetupHandler(meetupData) {
+        fetch('https://react-crash-course-tutorial-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers:{
+                'Content-Type': 'application/json'
+            
+                }
+            }
+        ).then(() => {
+            history.replace('/');
+        }); 
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={onAddMeetupHandler}/>
         </section>
     );
 }
